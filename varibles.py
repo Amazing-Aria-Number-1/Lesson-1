05e0fe0738795d2452d936f6a187ca25f9a84031
Amazing-Aria-Number-1 <1amhudani@gmail.com>
2025-10-14 20:41:25 -0400

    commit

---
+++ varibles.py
@@ -0,0 +1,11 @@
+name = "Aria"
+pie = 3.14
+food = "biryani"
+color = "blue"
+age = 12
+
+print("my favourite color is" , color )
+print("my name is " , name )
+print("I really like the number" , pie )
+print("my fav food is " , food )
+print("my age is " , age )
