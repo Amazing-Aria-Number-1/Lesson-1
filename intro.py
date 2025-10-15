c5ed176c92d98abac4f90910838e830e4ade9db5
Amazing-Aria-Number-1 <1amhudani@gmail.com>
2025-10-14 20:30:26 -0400

    commit

---
+++ intro.py
@@ -0,0 +1,9 @@
+name = "Aria"
+pets = 0
+color = "blue"
+number = 5 
+
+print("My name is " , name)
+print("I have " , pets , "amount of pets")
+print(" My favourite color is" , color )
+print("I really like the number" , number )
